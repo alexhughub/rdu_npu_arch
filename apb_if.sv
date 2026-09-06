87a7fc9e210aa2e878c81955478a884dfd6cd938
alexhughub <37986197+alexhughub@users.noreply.github.com>
2026-09-06 16:04:47 -0700

    Delete apb_if.sv

--- apb_if.sv
+++
@@ -1,3 +0,0 @@
-interface apb_if #(parameter AW=32, DW=32)(input logic PCLK, input logic PRESETn);
-  logic [AW-1:0] m_PADDR; logic m_PSEL,m_PENABLE,m_PWRITE; logic [DW-1:0] m_PWDATA,m_PRDATA; logic m_PREADY,m_PSLVERR;
-endinterface
